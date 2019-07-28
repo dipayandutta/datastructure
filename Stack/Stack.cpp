5e9844c908293c4148b01490f33459a27f7fb368
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-07-28 20:59:36 +0530

    Stack implementation using C++

---
+++ Stack/Stack.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+int stack[100] ;
+int n = 100 ;
+int top = -1;
+
+void push(int val){
+  if (top>=n-1){
+    cout << "Stack Overflow "<< endl;
+
+  }else{
+    top++;
+    stack[top] = val;
+  }
+}
+
+void pop(){
+  if(top<= -1){
+    cout << "Stack underflow ";
+    cout << endl;
+  }
+  else{
+    cout << "The popped element is "<< stack[top] << endl;
+    top --;
+  }
+}
+
+void display(){
+  if(top >=0){
+    cout << "Stack elements are: ";
+    cout << endl;
+    for(int i=top;i>=0;i--){
+      cout << stack[i] << " ";
+      cout << endl;
+    }
+  }
+  else{
+    cout << "Stack is empty";
+    cout << endl;
+  }
+}
+
+int main(){
+
+  int ch;
+  int val;
+
+  cout << "1) Push in Stack "<< endl;
+  cout << "2) Pop from Stacl "<< endl;
+  cout << "3) Display Stack " << endl;
+  cout <<"4) Exit "<< endl;
+
+  do {
+    cout << "Enter Choice "<< endl;
+    cin >> ch;
+
+    switch(ch){
+      case 1:{
+        cout << "Enter value to be pushed "<< endl;
+        cin >> val;
+        push(val);
+        break;
+
+      }
+      case 2:{
+               pop();
+               break;
+             }
+      case 3:{
+               display();
+               break;
+             }
+      case 4:{
+               cout << "Exit"<< endl;
+               break;
+             }
+      default:{
+                cout << "Invalid Choice "<< endl;
+              }
+    }
+  }while(ch!=4);
+
+  return 0;
+}
